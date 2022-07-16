eabb960827ef12c18ffda411d5f06f55015aa6df
syed <user.email>
2022-07-16 20:47:56 +0100

    added makkah data

--- src/components/placesData.tsx
+++ src/components/placesData.tsx
@@ -6,7 +6,7 @@ const places: placeInfo[] = [
     title: "Home Town",
     imageURL:
       "https://a.cdn-hotels.com/gdcs/production114/d1461/2267d2c2-21e8-4964-bed0-f5bb480def60.jpg?impolicy=fcrop&w=800&h=533&q=medium",
-    placeName: "Newcastle Upon Tyne",
+    placeName: "Newcastle upon Tyne",
     countryName: "United Kingdom",
     location:
       "https://www.google.com/maps/dir//Newcastle+upon+Tyne,+UK/@55.0023721,-1.6568434,12z/data=!4m17!1m7!3m6!1s0x487d857e0c6f64cd:0xbe252b072a76191!2sNewcastle+upon+Tyne,+UK!3b1!8m2!3d54.978252!4d-1.61778!4m8!1m0!1m5!1m1!1s0x487d857e0c6f64cd:0xbe252b072a76191!2m2!1d-1.61778!2d54.978252!3e3",
@@ -21,6 +21,26 @@ const places: placeInfo[] = [
     location:
       "https://www.google.com/maps?q=sylhet&source=lmns&bih=784&biw=1919&hl=en&sa=X&ved=2ahUKEwjQo7Dimf34AhXNhFwKHaY0BicQ_AUoAXoECAEQAQ",
     reason: `It's where my parents were born and grew up!`,
+  },
+  {
+    title: "The Holy Mosque",
+    imageURL:
+      "https://images.pexels.com/photos/4346403/pexels-photo-4346403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    placeName: "Masjid Al-Haram",
+    countryName: "Saudi Arabia",
+    location:
+      "https://www.google.com/maps/place/Mecca+Saudi+Arabia/@21.4360154,39.8465466,11z/data=!3m1!4b1!4m5!3m4!1s0x15c21b4ced818775:0x98ab2469cf70c9ce!8m2!3d21.3890824!4d39.8579118",
+    reason: `Most holy site in our religion, it was the very first mosque to be built on earth.`,
+  },
+  {
+    title: "The Holy Mosque",
+    imageURL:
+      "https://images.pexels.com/photos/4346403/pexels-photo-4346403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    placeName: "Masjid Al-Haram",
+    countryName: "Saudi Arabia",
+    location:
+      "https://www.google.com/maps?q=sylhet&source=lmns&bih=784&biw=1919&hl=en&sa=X&ved=2ahUKEwjQo7Dimf34AhXNhFwKHaY0BicQ_AUoAXoECAEQAQ",
+    reason: `Most holy site in our religion, it was the very first mosque to be built on earth.`,
   },
 ];
 
@@ -37,4 +57,4 @@ function mapToComponent(p: placeInfo): JSX.Element {
   );
 }
 
-export const placesComponent = places.map(mapToComponent);
+export const placesComponent: JSX.Element[] = places.map(mapToComponent);
